e5986393c3f4390370e9017328400f7d5081effe
Georgii Morgunov <georgii.morgunov@mail.ru>
2019-05-06 11:07:17 +0300

    package-info.java

--- chapter_001/src/main/ru/job4j/calculate/package-info.java
+++ chapter_001/src/main/ru/job4j/calculate/package-info.java
@@ -6,3 +6,21 @@
 * @since 0.1
 */
 package ru.job4j.calculate;
+
+/**
+ * Class Calculate выполнение задания.
+ * @author morgunov
+ * @since 05.05.2019
+*/
+public class Calculate {
+
+    /**
+     * Метод для тестирования.
+     * @param args - args строка для вывода в консоль
+     * @return "Hello World".
+    */
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+	}
+	
+}
