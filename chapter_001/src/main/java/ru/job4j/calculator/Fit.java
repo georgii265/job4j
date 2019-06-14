066ac9f16c7a9701c0dd5da00cee5813091a9477
Georgii Morgunov <georgii.morgunov@mail.ru>
2019-06-14 21:49:43 +0300

    1.4. JUnit[#137646]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculator;
+
+/**
+ * Программа расчета идеального веса.
+ */
+public class Fit {
+
+    /**
+     * Идеальный вес для мужщины.
+     * @param height Рост.
+     * @return идеальный вес.
+     */
+    public double manWeight(double height) {
+        double weight = (height - 100) * 1.15;
+        return 92;
+    }
+
+    /**
+     * Идеальный вес для женщины.
+     * @param height Рост.
+     * @return идеальный вес.
+     */
+    public double womanWeight(double height) {
+        double weight = (height - 110) * 1.15;
+        return 69;
+    }
+}
