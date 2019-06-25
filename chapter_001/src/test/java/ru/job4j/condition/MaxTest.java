9dacbc05959b6ca32a2ef04441a7ae2bdaf22117
Georgii Morgunov <georgii.morgunov@mail.ru>
2019-06-25 19:45:57 +0300

    1.4. JUnit[#137646]

--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -13,7 +13,7 @@ public class MaxTest {
     }
 
     @Test
-    public void whenMax2To1Then2() {
+    public void whenMax1To2Then1() {
         Max max = new Max();
         int result = max.max(2, 1);
         assertThat(result, is(1));
