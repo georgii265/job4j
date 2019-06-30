470c643ddfb76530ddcd574e6ba4771f4a1fbfb4
Georgii Morgunov <georgii.morgunov@mail.ru>
2019-06-30 15:56:15 +0300

    1.4. JUnit[#137646]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -1,12 +1,15 @@
 package ru.job4j.condition;
 
 public class Max {
+    /**
+     * Method max.
+     * Находит максимальное из двух чисел.
+     *
+     * @param left  - Первое число.
+     * @param right - Второе число.
+     * @return Значение максимального из двух чисел.
+     */
     public int max(int left, int right) {
-        int max;
-        if (left < right)
-            max = right;
-        else
-            max = left;
-        return max;
+        return left > right ? left : right;
     }
 }
