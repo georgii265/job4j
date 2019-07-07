d5fa0b6f0207a3d4ea8dd5fd04b92f7fda4cfd60
Georgii Morgunov <georgii.morgunov@mail.ru>
2019-07-07 23:22:55 +0300

    1.4. JUnit[#137646]

--- chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -8,8 +8,8 @@ import static org.junit.Assert.assertThat;
 public class CounterTest {
     @Test
     public void whenSumEvenNumbersFromOneToTenThenThirty() {
-        Counter c = new Counter();
-        int result = c.add  (0, 10);
+        Counter a = new Counter();
+        int result = a.add  (0, 10);
         int expected = 30;
         assertThat(result, is(expected));
         //напишите здесь тест, проверяющий, что сумма чётных чисел от 1 до 10 при вызове метода counter.add будет равна 30.
