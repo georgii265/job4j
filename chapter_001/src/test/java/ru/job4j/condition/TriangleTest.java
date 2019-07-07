1faee17db971a79415f17905706d320f7a2de59d
Georgii Morgunov <georgii.morgunov@mail.ru>
2019-07-07 21:08:27 +0300

    1.4. JUnit[#137646]

--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -11,9 +11,13 @@ import static org.junit.Assert.*;
 public class TriangleTest {
     @Test
     public void whenAreaSetThreePointsThenTriangleArea() {
+        // Создаем объект треугольник.
         Triangle triangle = new Triangle();
+        // Вычисляем площадь.
         double result = triangle.area(0, 0, 0, 2, 2, 0);
+        // Задаем ожидаемый результат.
         double expected = 2D;
+        //Проверяем результат и ожидаемое значение.
         assertThat(result, closeTo(expected, 0.1));
     }
 }
