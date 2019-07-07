b6919500b7cfacb53c767a1c81adb71dfbd7fa03
Georgii Morgunov <georgii.morgunov@mail.ru>
2019-07-07 20:56:22 +0300

    1.4. JUnit[#137646]

--- chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
@@ -8,21 +8,21 @@ public class MultiMaxTest {
     @Test
     public void whenSecondMax() {
         MultiMax check = new MultiMax();
-        int result = check.max(1, 4, 2);
+        int result = check.max(4, 2, 1);
         assertThat(result, is(4));
     }
 
     @Test
     public void whenFirstMax() {
         MultiMax check = new MultiMax();
-        int result = check.max(1, 4, 2);
+        int result = check.max(2, 1, 4);
         assertThat(result, is(4));
     }
 
     @Test
     public void whenThirdMax() {
         MultiMax check = new MultiMax();
-        int result = check.max(1, 4, 2);
+        int result = check.max(4, 4, 4);
         assertThat(result, is(4));
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -11,13 +11,9 @@ import static org.junit.Assert.*;
 public class TriangleTest {
     @Test
     public void whenAreaSetThreePointsThenTriangleArea() {
-        // Создаем объект треугольник.
         Triangle triangle = new Triangle();
-        // Вычисляем площадь.
         double result = triangle.area(0, 0, 0, 2, 2, 0);
-        // Задаем ожидаемый результат.
         double expected = 2D;
-        //Проверяем результат и ожидаемое значение.
         assertThat(result, closeTo(expected, 0.1));
-    } // как я понимаю т.е это (Создаем объект треугольник = Triangle triangle = new Triangle();).И так далее,так или нет?
-} // здесь же необходимо еще добавить два теста,если да то что он должен проверять?
+    }
+}
