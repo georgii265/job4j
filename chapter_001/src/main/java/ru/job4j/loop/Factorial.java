47256edd30c2bbc9c5d00453f090efd30cc9d6b7
Georgii Morgunov <georgii.morgunov@mail.ru>
2019-07-10 22:44:14 +0300

    1.4. JUnit[#137646]

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -2,13 +2,11 @@ package ru.job4j.loop;
 
  public class Factorial {
      public int calc(int n) {
-        int result = 1;
-        int i;
-        for (i = 1; i <= n; i++) {
-            result = result * i;
-            if (n == 0) {
-            }
-        }
-        return result;
+         int result = 1;
+         int i;
+         for (i = 1; i <= n; i++) {
+             result = result * i;
+         }
+         return result;
     }
 }
