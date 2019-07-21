82e1e414e20470c31b864a1fc9c104212a6cd3a1
Georgii Morgunov <georgii.morgunov@mail.ru>
2019-07-21 21:13:24 +0300

    1.4. JUnit[#137646]

--- chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ArrayCharTest {
+
     @Test
     public void whenStartWithPrefixThenTrue() {
         ArrayChar word = new ArrayChar("Hello");
