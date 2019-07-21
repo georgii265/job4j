48bf72db8b1790ac885ec0e6cf30b253900cf45a
Georgii Morgunov <georgii.morgunov@mail.ru>
2019-07-21 11:39:54 +0300

    1.4. JUnit[#137646]

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class Check {
+    public boolean mono(boolean[] data) {
+        boolean result = true;
+        {
+            for (int index = 0; index < data.length; index++)
+                if (data[0] != data[index]) {
+                    result = false;
+                    break;
+                }
+        }
+
+        return result;
+    }
+}
