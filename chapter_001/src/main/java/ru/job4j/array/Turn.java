12ce9f625a9429f8387d7b3a96653bcb6c97cb87
Georgii Morgunov <georgii.morgunov@mail.ru>
2019-07-21 11:24:27 +0300

    1.4. JUnit[#137646]

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class Turn {
+    public int[] back(int[] array) {
+        for (int index = 0; index < array.length / 2; index++) {
+                    int temp = array[index];
+                    array[index] = array[array.length - 1 - index];
+                    array[array.length - 1 - index] = temp;
+        }
+        return array;
+    }
+}
