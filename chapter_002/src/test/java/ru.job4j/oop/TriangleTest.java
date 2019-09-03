2cc3ac0d555a20f02737847d450a2a8c5335f208
Georgii Morgunov <georgii.morgunov@mail.ru>
2019-09-03 10:14:08 +0300

    Рефакторинг - Площадь треугольника.

---
+++ chapter_002/src/test/java/ru.job4j/oop/TriangleTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.oop;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+
+public class TriangleTest {
+    @Test
+    public void Point () {
+        //Создадим три поля с типом Point.
+        Point first = new Point(0,0);
+        Point second = new Point(0,2);
+        Point third = new Point (2,0);
+        double result = first.distance(second);
+        first.info();
+        second.info();
+        third.info();
+        System.out.println(String.format("Result is %s", result));
+        assertThat(result, is(2D));
+    }
+
+    @Test
+    public void period() {
+        Point point = new Point(0, 0);
+        double result = point.distance(point);
+        assertThat(result, is(0.0));
+    }
+
+    @Test
+    public void area() {
+        Point first = new Point(0,0);
+        first.info();
+        Point second = new Point(0,2);
+        second.info();
+        Point third = new Point (2,0);
+        third.info();
+    }
+}
