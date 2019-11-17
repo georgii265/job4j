911d20a22a8fd02364e7ea23f7f720d7a094f202
Georgii Morgunov <georgii.morgunov@mail.ru>
2019-11-17 14:56:28 +0300

    Создайте модуль chapter_002

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,9 +1,10 @@
 package ru.job4j.tracker;
 
 /*Создайте модуль chapter_002/pom.xml
- *Создайте класс chapter_002/src/main/java/ru/job4j/tracker/Tracker.java. Класс должен быть пустой.
- *Залейте код.
- *Переведите задания на Петра Арсентьева.
+ * Создайте класс chapter_002/src/main/java/ru/job4j/tracker/Tracker.java.
+ * Класс должен быть пустой.
+ * Залейте код.
+ * Переведите задания на Петра Арсентьева.
  */
 public class Tracker {
 
