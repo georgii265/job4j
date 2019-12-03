5d8d0628ffb7ed7a47977e5af79d225c99ae5b90
Georgii Morgunov <georgii.morgunov@mail.ru>
2019-12-03 23:18:58 +0300

    Перегрузка конструктора.

--- chapter_002/src/main/java/ru/job4j/tracker/Animal.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Animal.java
@@ -1,7 +1,6 @@
 package ru.job4j.tracker;
 
 public class Animal {
-
     /*
      * 2. Перегрузка конструктора. [#173890]
      * Вернитесь к заданию про конструкторы по умолчанию и добавьте в базовый класс Animal поле String name,
@@ -10,13 +9,12 @@ public class Animal {
      */
     private String name;
 
-
     public Animal(){
 
     }
 
     public Animal(String name) {
-
+           this.name = name;
     }
 
     public static void main(String[] args) {
--- chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
@@ -1,6 +1,8 @@
 package ru.job4j.tracker;
 
+
 public class Tiger extends Animal {
+
     public Tiger(String name) {
         super(name);
     }
