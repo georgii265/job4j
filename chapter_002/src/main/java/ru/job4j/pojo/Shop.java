da29f91cee2412053dc45f238c376a50612b219b
Georgii Morgunov <georgii.morgunov@mail.ru>
2019-12-22 18:03:58 +0300

    Удаление моделей из массива.

--- chapter_002/src/main/java/ru/job4j/pojo/Shop.java
+++ chapter_002/src/main/java/ru/job4j/pojo/Shop.java
@@ -1,5 +1,7 @@
 package ru.job4j.pojo;
 
+import java.util.Arrays;
+
 public class Shop {
     /**
      * 1. Напишите метод в классе ru.job4j.pojo.Shop.
@@ -11,24 +13,31 @@ public class Shop {
      * 2. Добавьте демонстрацию работы примера в метод main.
      */
     public static Product[] delete(Product[] products, int index) {
+
+        /**
+         * Java предоставляет встроенный метод System.arraycopy() для копирования элементов из одного массива в другой.
+         */
         products[index] = null;
-        for (int i = index; i < products.length; i++) {
-            products[i] = products[i + 1];
+        for (int i = index; i < products.length -1; i++) {
+             products[i] = products[i + 1];
         }
-        products[products.length - 1] = null;
-        return products;
+        //products[products.length - 1] = null;
+        //return products;
+        return Arrays.copyOf(products, products.length - 1);
     }
+
 
     public static void main(String[] args) {
 
         Product products[] = new Product[5];//создание массива.
-        //new Shop().delete(products, 5); можно еще таким способом вызвать.
-        delete(products,5);// вызываем метод удаления.
+
         products[0] = new Product("Молоко", 10);
         products[1] = new Product("Лимонад", 15);
         products[2] = new Product("Мороженое", 20);
         products[3] = new Product("Яйцо", 18);
         products[4] = new Product("Гречка", 23);
+        delete(products,2);// вызываем метод удаления.
+        //new Shop().delete(products, 5); можно еще таким способом вызвать.
 
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -8,5 +8,33 @@ package ru.job4j.tracker;
  *Переведите задания на Петра Арсентьева.
  */
 public class Tracker {
+    /**
+     * Массив для хранения заявок.
+     */
+    private final Item[] items = new Item[100];
 
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    /**
+     * Метод добавления заявки в хранилище
+     * @param item новая заявка
+     */
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        items[this.position++] = item;
+        return item;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        //Реализовать метод генерации.
+        return null;
+    }
 }
