c8c3963597d049c60fbd18d6d6620ae1664bdcb7
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-01-22 22:22:27 +0300

    Метод замены заявки. Tracker.replace.

---
+++ chapter_002/src/main/java/ru/job4j/ru/job4j/io/Greeting.java
@@ -0,0 +1,26 @@
+package ru.job4j.ru.job4j.io;
+
+import java.util.Scanner;
+
+/**
+ * мы собираемся использовать объект класса Scanner, нам нужно сперва импортировать этот класс.
+ * import java.util.Scanner;
+ * Сделаем программу которая просит ввести имя и поприветствует нас.
+ * Greeting - приветствие.
+ *  input - вход.
+ *  nextLine - следующая строка.
+ */
+public class Greeting {
+    public static void main(String[] args) {
+        //нужно создать объект Scanner.
+        //Он принимает в качестве параметра объект "System.in".
+        Scanner input = new Scanner(System.in);
+        System.out.print("Добро пожаловать. Как Вас зовут? ");
+        //У объекта Scanner есть метод nextLine(),
+        //который считывает все символы строки после нажатия на клавишу Enter.
+        //После того, как пользователь нажал кнопку Enter,
+        // программа записывает введенные символы в переменную и продолжает выполнение программы.
+        String name = input.nextLine();
+        System.out.println(name + ", рад Вас видеть!");
+    }
+}
