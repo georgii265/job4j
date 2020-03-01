91c5cb71ca9c4e6bfb2ce8d18ed86ef1c7d16ecf
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-03-01 21:28:40 +0300

    Реализация класса StartUI.

---
+++ chapter_002/src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,26 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    public static void main(String[] args) {
+
+    }
+    @Override
+    public void go() {
+       Bus bus = new Bus();
+       bus.go();
+
+    }
+
+    @Override
+    public void passengers(int passengers) {
+        Bus bus = new Bus();
+        bus.passengers(10);// количество пассажиров
+    }
+
+    @Override
+    public int fillUp(int fuelbus) {
+        Bus bus = new Bus();
+        bus.fillUp(50); // емкость топливного бака
+        return 20;
+    }
+}
