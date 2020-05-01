37459adf1086f92fd006b1a4e4321e8d8f288fcb
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-05-01 18:52:47 +0300

    Разрыв зависимости StartUI от Scanner.

---
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker;
+
+import java.util.Scanner;
+
+/**
+ * Давайте мы сделаем новый класс, который реализует интерфейс Input и который внутри будет работать с Scanner.
+ */
+public class ConsoleInput implements Input {
+        private Scanner scanner = new Scanner(System.in);
+
+        @Override
+        public String askStr(String question) {
+            System.out.print(question);
+            return scanner.nextLine();
+        }
+
+        @Override
+        public int askInt(String question) {
+            return Integer.valueOf(askStr(question));
+        }
+    }
+
