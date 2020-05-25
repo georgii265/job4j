cf93f99ce7b67ab533574c30a23b3c0a677d4939
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-05-25 21:01:37 +0300

    Аргументы.

--- chapter_002/src/main/java/ru/job4j/stragery/Paint.java
+++ chapter_002/src/main/java/ru/job4j/stragery/Paint.java
@@ -4,6 +4,7 @@ package ru.job4j.stragery;
  * В классе Paint реализовать метод public void draw(Shape shape).
  * В классе Paint реализовать метод публичного рисования пустот (Shape shape).
  * Метод draw() должен печатать псевдокартинку на консоль через метод System.out.println(shape.draw()).
+ * При вызове paint.draw (new Trianlge ()) в консоли должен быть напечатан треугольник.
  */
 public class Paint {
     public void draw(Shape shape) {
