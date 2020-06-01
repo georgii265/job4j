4070355b8c960022ccae8055b171ea382b25b4eb
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-06-01 21:03:08 +0300

    Рефакторинг теста @Before @After

--- chapter_002/src/main/java/ru/job4j/stragery/Paint.java
+++ chapter_002/src/main/java/ru/job4j/stragery/Paint.java
@@ -8,7 +8,7 @@ package ru.job4j.stragery;
  */
 public class Paint {
     public void draw(Shape shape) {
-        System.out.println(shape.draw());
+        System.out.print(shape.draw());
     }
     public static void main(String[] args) {
         Paint paint = new Paint();
