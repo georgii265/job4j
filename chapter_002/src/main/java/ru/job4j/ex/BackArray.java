dfa70a5c42d674fe3a4f09b802401bf0148a7827
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-06-03 00:02:02 +0300

    Что такое исключение.

---
+++ chapter_002/src/main/java/ru/job4j/ex/BackArray.java
@@ -0,0 +1,17 @@
+package ru.job4j.ex;
+
+import java.util.Arrays;
+
+public class BackArray {
+    public static void main(String[] args) {
+        String[] names = {"Petr", "Ivan", "Nik", "Vasya"};
+        int middle = names.length / 2;
+        for (int index = 0; index < middle; index++) {
+            String temp = names[index];
+            names[middle] = names[names.length - middle];
+            names[names.length - middle] = temp;
+        }
+            System.out.println(Arrays.toString(names));
+        }
+    }
+
