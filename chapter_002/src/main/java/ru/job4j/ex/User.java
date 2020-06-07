e5bdb30dc09fe0e06e7c0aaf4aa4a78a00220538
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-06-07 23:25:07 +0300

    Error - исключения, связанные с работой виртуальной машины.

--- chapter_002/src/main/java/ru/job4j/ex/User.java
+++ chapter_002/src/main/java/ru/job4j/ex/User.java
@@ -4,6 +4,7 @@ public class User {
     private String username;
     private boolean valid;
 
+
     public User(String username, boolean valid) {
         this.username = username;
         this.valid = valid;
