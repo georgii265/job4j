a1766c4fbcc2fb76ea88d951b028c3185d714a89
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-06-16 19:46:14 +0300

    Элементарный калькулятор.

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -2,19 +2,19 @@ package ru.job4j.calculator;
 
 public class Calculator {
 
-    public double add(double first, double second) {
-        return first + second;
-    }
-
-    public double subtract(double first, double second) {
-        return first - second;
-    }
-
-    public double div(double first, double second) {
-        return first / second;
-    }
-
-    public double multiple(double first, double second) {
-        return first * second;
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int onePlusTwo = one + two;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
+        System.out.println(onePlusTwo);
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(fourTimeTwo);
     }
 }
