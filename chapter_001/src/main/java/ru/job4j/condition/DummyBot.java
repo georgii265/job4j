1facff0d62ce4ca76a6f2def8829a89cc43a654d
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-06-17 16:14:09 +0300

    Глупый бот.

--- chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -1,27 +1,21 @@
 package ru.job4j.condition;
 
-/**
- * @author Georgii Morgunov (georgii.morgunov@mail.ru)
- * @version $Id$
- * @since 0.1
- */
 public class DummyBot {
-    /**
-     * Отвечает на вопросы.
-     *
-     * @param question Вопрос от клиента.
-     * @return
-     */
-    public String answer(String question) {
+
+    public static String answer(String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
-        // заменить ... на правильный ответ rsl = "ответ по заданию".
         if ("Привет, Бот.".equals(question)) {
             rsl = "Привет, умник.";
-        } else if ("Пока.".equals(question)) { // заменить ... на проверку, известен ли боту этот вопрос и он знает как на него ответить.
-            // заменить ... на правильный ответ rsl = "ответ по заданию".
+        } else if ("Пока.".equals(question)) {
             rsl = "До скорой встречи.";
         }
         return rsl;
+    }
+    public static void main(String[] args) {
+        String rsl = DummyBot.answer("Привет, Бот");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Пока.");
+        System.out.println(rsl);
     }
 }
 
