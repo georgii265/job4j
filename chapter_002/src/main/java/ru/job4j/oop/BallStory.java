e5555eaaba624670f254acc260e858c56b084168
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-06-19 20:41:18 +0300

    Взаимодействие объектов.

--- chapter_002/src/main/java/ru/job4j/oop/BallStory.java
+++ chapter_002/src/main/java/ru/job4j/oop/BallStory.java
@@ -1,11 +1,7 @@
 package ru.job4j.oop;
 
 public class BallStory {
-    /**
-     *  Здесь мы создали три объекта,
-     *  а ниже вызвали методы у объектов и передали в них другие объекты.
-     * @param args
-     */
+
     public static void main(String[] args) {
         Ball ball = new Ball();
         Wolf wolf = new Wolf();
