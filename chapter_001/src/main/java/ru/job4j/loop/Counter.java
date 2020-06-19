cedf8ea2ecc2064fc96042babecdd1b29b84b87c
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-06-19 19:30:58 +0300

    Подсчет суммы чётных чисел в диапазоне.

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -2,14 +2,31 @@ package ru.job4j.loop;
 
 public class Counter {
 
-    public int add(int start, int finish ) {
-        int result = 0;
-        int index;
-        for (index = start; index <= finish; index++) {
+    public static int sum(int start, int finish) {
+        int sum = 0;
+        for (int index = start; index <= finish; index++) {
+            sum = sum + index;
+        }
+        return sum;
+    }
+
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int index = start; index <= finish; index++) {
             if (index % 2 == 0) {
-                result = result + index;
+                sum = sum + index;
             }
         }
-        return result;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
+
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
     }
 }
--- chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -9,9 +9,8 @@ public class CounterTest {
     @Test
     public void whenSumEvenNumbersFromOneToTenThenThirty() {
         Counter a = new Counter();
-        int result = a.add  (0, 10);
+        int result = a.sumByEven(1, 10);
         int expected = 30;
         assertThat(result, is(expected));
-        //напишите здесь тест, проверяющий, что сумма чётных чисел от 1 до 10 при вызове метода counter.add будет равна 30.
     }
 }
