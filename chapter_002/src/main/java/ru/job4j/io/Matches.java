8c4d3e23c45087a16df8004a28c7f0c011e1e893
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-06-20 23:13:45 +0300

    Scanner и чтение числа из консоли.

--- chapter_002/src/main/java/ru/job4j/io/Matches.java
+++ chapter_002/src/main/java/ru/job4j/io/Matches.java
@@ -1,20 +1,7 @@
 package ru.job4j.io;
 
 import java.util.Scanner;
-/**
- * 1. Создайте класс ru.job4j.io.Matches с методом main.
- * 2. В методе main добавьте цикл. Игра заканчивается, когда игрок забрал последние спички.
- *    В цикле нужно проверять количество спичек.
- * 3. Сделайте ввод данных от пользователя.
- *    По порядку. Сначала первый игрок вводит число от 1 до 3, потом второй и так далее.
- *    Когда игрок ввел цифру, нужно показать сколько спичек осталось на столе.
- *
- *   Двое игроков берут по очереди спички со стола.
- *   Каждый из игроков может взять одну, две, или три спички.
- *   Не взять ни одной нельзя, также как и взять более трех спичек.
- *   Выигрывает тот игрок, который брал спички со стола последним.
- *   if (igrok % 2 == 0) { //счетчик количества ходов победителя
- */
+
 public class Matches {
 
     public static void main(String[] args) {
@@ -22,18 +9,19 @@ public class Matches {
         int match = 11;
         int igrok = 0;
         while (match > 0) {
+            int igrokName = igrok % 2 + 1;
             System.out.println("Количество спичек на столе: " + match);
             System.out.print("Выберите количество спичек от 1 до 3: \n");
             int select = Integer.valueOf(input.nextLine());
             igrok++;
             match -= select;
-            System.out.println();
+            System.out.println(igrokName + " игрок взял " + select + " спичек");
         }
         if (igrok % 2 == 0) {
-            System.out.println("first");
+            System.out.print("Антон");
         } else {
-            System.out.println("second");
+            System.out.print("Иван");
         }
-        System.out.println();
+        System.out.print(" победил!");
     }
 }
