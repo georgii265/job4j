90bd1902777d2213e8e5a4cebe7f48e95f8feb36
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-06-23 18:15:51 +0300

    Перегрузить метод max для трех чисел.

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 public class Max {
 
     public int max(int left, int right) {
-        return left < right ? left : right;
+        return left < right ? right : left;
     }
 
     public int max(int first, int second, int third, int fourth) {
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -11,13 +11,13 @@ public class MaxTest {
     public void max() {
         Max max = new Max();
         int result = max.max(1, 2, 3, 4);
-        assertThat(result, is(1));
+        assertThat(result, is(4));
     }
 
     @Test
     public void whenMaxFo3NumbersMaxFo4Numbers() {
         Max max = new Max();
         int result = max.max(1, 2, 3,4);
-        assertThat(result, is(1));
+        assertThat(result, is(4));
     }
 }
