1e4f1c65d6d9ed8bf987bc38b6f11b19891f8d20
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-06-24 16:03:56 +0300

    Перегрузка конструктора.

--- chapter_002/src/main/java/ru/job4j/max/Reduce.java
+++ chapter_002/src/main/java/ru/job4j/max/Reduce.java
@@ -1,11 +1,18 @@
 package ru.job4j.max;
 
 public class Reduce {
+    /**
+     * Как правило, ключевое слово this в Java используется для:
+     * дифференцирования между переменными экземпляра и локальными переменными в случае,
+     * если у них одинаковые имена, в составе конструктора или метода.
+     */
+    private int[] array;// переменная которая принадлежит обьекту изначально int[] array = null
 
-    private int[] array;
-
-    public void to(int[] array) {
-        this.array = array;
+    public void to(int[] array) {// переменная принадлежит методу
+        // array1 = array; здесь получается что мы присваеваем значение переменной array переменной int[] array,
+        // что было бы ни верно.
+        this.array = array;// this.array - это private int[] array;
+        // теперь у нас получается что array: {1,2,3}
     }
 
     public void print() {
