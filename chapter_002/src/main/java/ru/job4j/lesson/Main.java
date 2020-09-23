2cad2b5e5cbaee267a4c0cd6d8984966a6ccfbe0
Georgii Morgunov <georgii.morgunov@mail.ru>
2020-09-23 20:51:57 +0300

    Checkstyle с Maven.Изменения.

---
+++ chapter_002/src/main/java/ru/job4j/lesson/Main.java
@@ -0,0 +1,16 @@
+package ru.job4j.lesson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        System.out.println("Мы считали с клавиатуры эту строку: ");
+        System.out.println(s);
+    }
+}
